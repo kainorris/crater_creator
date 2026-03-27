159fd2713d935cadf6de79f672f567ffd564b743
Aolgeirson <asher@olgeirson.com>
2026-03-27 12:34:48 -0400

    unsmashed the stack

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -5,11 +5,12 @@
 #include "vl53l1_ll_device.h"
 #include "vl53l1_platform.h"
 #include <stdint.h>
+#include <stdlib.h>
 
 #define TAG "main or something"
 
 int app_main() {
-
+  ESP_LOGI(TAG, "mark 1\n");
   i2c_master_bus_config_t i2c_mst_config = {
       .clk_source = I2C_CLK_SRC_DEFAULT,
       .i2c_port = 0,
@@ -19,30 +20,38 @@ int app_main() {
       .flags.enable_internal_pullup = true,
   };
 
+  ESP_LOGI(TAG, "mark 2\n");
   i2c_master_bus_handle_t bus_handle;
+  ESP_LOGI(TAG, "mark 3\n");
   ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+  ESP_LOGI(TAG, "mark 4\n");
+  VL53L1_Dev_t *dev =
+      malloc(sizeof(VL53L1_Dev_t)); // Heap allocated to prevent stack smashing
 
-  VL53L1_Dev_t dev = {
-      .bus_handle = &bus_handle,
-      .i2c_slave_address = 0x52,
-      .comms_speed_khz = 400
-      // this might crash I guess
-  };
-  VL53L1_WaitDeviceBooted(&dev);
-  if (VL53L1_CommsInitialise(&dev, VL53L1_I2C, 400)) {
-    ESP_LOGE(TAG, "Lidar Sensor VL53L1X [OK]");
+  dev->bus_handle = &bus_handle;
+  dev->i2c_slave_address = 0x52;
+  dev->comms_speed_khz = 400;
+  // this might crash I guess
+  ESP_LOGI(TAG, "mark 5\n");
+  VL53L1_WaitDeviceBooted(dev);
+  ESP_LOGI(TAG, "mark 6\n");
+  if (VL53L1_CommsInitialise(dev, VL53L1_I2C, 400)) {
+    ESP_LOGE(TAG, "Lidar Sensor VL53L1X [OK]\n");
   } else {
-    ESP_LOGE(TAG, "Lidar Sensor VL53L1X [FAIL]");
+    ESP_LOGE(TAG, "Lidar Sensor VL53L1X [FAIL]\n");
     return 1;
   }
+  ESP_LOGI(TAG, "mark 7\n");
 
-  VL53L1_StopMeasurement(&dev);
-  VL53L1_SetDistanceMode(&dev, VL53L1_DISTANCEMODE_MEDIUM);
-  VL53L1_SetMeasurementTimingBudgetMicroSeconds(&dev, 25000);
+  VL53L1_StopMeasurement(dev);
+  ESP_LOGI(TAG, "mark 8\n");
+  VL53L1_SetDistanceMode(dev, VL53L1_DISTANCEMODE_MEDIUM);
+  ESP_LOGI(TAG, "mark 9\n");
+  VL53L1_SetMeasurementTimingBudgetMicroSeconds(dev, 25000);
 
   // while (1) {
 
-  VL53L1_StartMeasurement(&dev);
+  VL53L1_StartMeasurement(dev);
 
   //   while (dataReady == 0) {
   //     status = VL53L1_GetMeasurementDataReady(&dev, &dataReady);
